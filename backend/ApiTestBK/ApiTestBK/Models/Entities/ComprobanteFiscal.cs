293afb0ebb515bff6f4623310c94d7cc06a626fd
Alejandro Cortorreal <acortorreal@911.gob.do>
2025-08-15 22:00:33 -0400

    Creando proyecto, dependencias y modelos

---
+++ backend/ApiTestBK/ApiTestBK/Models/Entities/ComprobanteFiscal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiTestBK.Models.Entities
+{
+    public class ComprobanteFiscal
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string RncCedula { get; set; } //fk contribuyente
+        public string? NCF { get; set; }
+        public decimal Monto { get; set; }
+        public decimal Itbis18 { get; set; }
+
+        //Propiedad de navegacion opcional en EF
+        [ForeignKey("RncCedula")]
+        public Contribuyente Contribuyente { get; set; }
+    }
+}
---
+++ backend/ApiTestBK/ApiTestBK/Models/Entities/Contribuyente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiTestBK.Models.Entities
+{
+    public class Contribuyente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string RncCedula { get; set; }
+        public required string Nombre { get; set; }
+        public string? Tipo { get; set; } //en caso de que sea opcional
+        public string? Estatus {get; set; }
+
+        //Relacion
+        public List<ComprobanteFiscal> ComprobantesFiscales { get; set; } = new List<ComprobanteFiscal>();
+    }
+}
